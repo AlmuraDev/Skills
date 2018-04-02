1e831f69a9ee3aaf306d30652baa60c7d08b8a9d
Chris Sanders <zidane@spongepowered.org>
2018-04-02 11:47:56 -0500

    BlockFilter.
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/org/inspirenxe/skills/impl/component/filter/FilterModule.java
+++ src/main/java/org/inspirenxe/skills/impl/component/filter/FilterModule.java
@@ -27,6 +27,7 @@ package org.inspirenxe.skills.impl.component.filter;
 import com.almuradev.droplet.component.filter.FilterBinder;
 import com.almuradev.droplet.parser.ParserBinder;
 import net.kyori.violet.AbstractModule;
+import org.inspirenxe.skills.impl.component.filter.block.BlockFilterParser;
 import org.inspirenxe.skills.impl.component.filter.key.NamespaceFilterParser;
 import org.inspirenxe.skills.impl.component.filter.key.RegistryKeyFilterParser;
 
@@ -36,5 +37,6 @@ public final class FilterModule extends AbstractModule implements FilterBinder, 
   protected void configure() {
     this.bindFilter("key").to(RegistryKeyFilterParser.class);
     this.bindFilter("namespace").to(NamespaceFilterParser.class);
+    this.bindFilter("block").to(BlockFilterParser.class);
   }
 }
