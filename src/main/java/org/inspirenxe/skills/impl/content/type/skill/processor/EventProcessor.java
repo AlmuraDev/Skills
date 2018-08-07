44296b6255e5db5ea8792856e15713bfba0f2b42
Aaron Hill <aa1ronham@gmail.com>
2018-08-07 15:30:05 -0400

    Don't create duplicate EventScripts for nested event elements

--- src/main/java/org/inspirenxe/skills/impl/content/type/skill/processor/EventProcessor.java
+++ src/main/java/org/inspirenxe/skills/impl/content/type/skill/processor/EventProcessor.java
@@ -50,107 +50,114 @@ import java.util.stream.Collectors;
 
 public final class EventProcessor implements Processor<ContentSkillTypeBuilder> {
 
-  private final Registry<EventType> eventTypeRegistry;
-  private final Parser<Filter> filterParser;
-  private final Parser<EventApplicator> eventApplicatorParser;
+    private final Registry<EventType> eventTypeRegistry;
+    private final Parser<Filter> filterParser;
+    private final Parser<EventApplicator> eventApplicatorParser;
 
-  @Inject
-  public EventProcessor(final Registry<EventType> eventTypeRegistry, final Parser<Filter> filterParser, final Parser<EventApplicator> eventApplicatorParser) {
-    this.eventTypeRegistry = eventTypeRegistry;
-    this.filterParser = filterParser;
-    this.eventApplicatorParser = eventApplicatorParser;
-  }
-
-  @Override
-  public void process(final Node node, final ContentSkillTypeBuilder builder) {
-
-    for (final EventType eventType : this.eventTypeRegistry.all()) {
-      final PathNodeFlattener flattener = new PathNodeFlattener(NodeFilters.onlyElements(), eventType.getPath());
-
-      flattener
-        .flatten(node)
-        .forEach(eventTypeNode -> {
-          final EventScript.Builder eventScriptBuilder = builder.eventScript(eventType);
-
-          eventTypeNode
-            .nodes("apply")
-            .collect(Collectors.toList())
-            .forEach(applyNode -> {
-              Branch.Builder branchBuilder = null;
-              final Node ifNode = applyNode.nodes("if").collect(MoreCollectors.toOptional()).orElse(null);
-              if (ifNode != null) {
-                branchBuilder = IfBranch.builder();
-                this.traverseIf(applyNode, (IfBranch.Builder) branchBuilder);
-              }
-
-              if (branchBuilder != null) {
-                eventScriptBuilder.branch(branchBuilder.build());
-              }
-            });
-        });
-    }
-  }
-
-  private void traverseIf(final Node root, final IfBranch.Builder rootBuilder) {
-    final Node ifNode = root.nodes("if").collect(MoreCollectors.onlyElement());
-    if (ifNode.nodes().stream().count() == 0) {
-      rootBuilder.statement(this.filterParser.parse(ifNode));
-    } else {
-      rootBuilder.statement(this.filterParser.parse(ifNode.nodes().collect(MoreCollectors.onlyElement())));
+    @Inject
+    public EventProcessor(final Registry<EventType> eventTypeRegistry, final Parser<Filter> filterParser, final Parser<EventApplicator> eventApplicatorParser) {
+        this.eventTypeRegistry = eventTypeRegistry;
+        this.filterParser = filterParser;
+        this.eventApplicatorParser = eventApplicatorParser;
     }
 
-    final ThenBranch.Builder thenBranchBuilder = ThenBranch.builder();
-    final Node thenNode = root.nodes("then").collect(MoreCollectors.onlyElement());
-    this.parseApplicators(thenNode, thenBranchBuilder);
-    rootBuilder.branch(thenBranchBuilder.build());
+    @Override
+    public void process(final Node node, final ContentSkillTypeBuilder builder) {
+
+        for (final EventType eventType : this.eventTypeRegistry.all()) {
+            final PathNodeFlattener flattener = new PathNodeFlattener(NodeFilters.onlyElements(), eventType.getPath());
+
+            flattener
+                    .flatten(node)
+                    .forEach(eventTypeNode -> {
+                        // Don't build an event script unless we have at least one 'apply'
+                        // node. This ensures that we don't build EventScripts for event nodes
+                        // that only contain child events
+                        final EventScript.Builder[] eventScriptBuilder = {null};
+
+                        eventTypeNode
+                                .nodes("apply")
+                                .collect(Collectors.toList())
+                                .forEach(applyNode -> {
+                                    if (eventScriptBuilder[0] == null) {
+                                        eventScriptBuilder[0] = builder.eventScript(eventType);
+                                    }
+
+                                    Branch.Builder branchBuilder = null;
+                                    final Node ifNode = applyNode.nodes("if").collect(MoreCollectors.toOptional()).orElse(null);
+                                    if (ifNode != null) {
+                                        branchBuilder = IfBranch.builder();
+                                        this.traverseIf(applyNode, (IfBranch.Builder) branchBuilder);
+                                    }
+
+                                    if (branchBuilder != null) {
+                                        eventScriptBuilder[0].branch(branchBuilder.build());
+                                    }
+                                });
+                    });
+        }
+    }
+
+    private void traverseIf(final Node root, final IfBranch.Builder rootBuilder) {
+        final Node ifNode = root.nodes("if").collect(MoreCollectors.onlyElement());
+        if (ifNode.nodes().stream().count() == 0) {
+            rootBuilder.statement(this.filterParser.parse(ifNode));
+        } else {
+            rootBuilder.statement(this.filterParser.parse(ifNode.nodes().collect(MoreCollectors.onlyElement())));
+        }
+
+        final ThenBranch.Builder thenBranchBuilder = ThenBranch.builder();
+        final Node thenNode = root.nodes("then").collect(MoreCollectors.onlyElement());
+        this.parseApplicators(thenNode, thenBranchBuilder);
+        rootBuilder.branch(thenBranchBuilder.build());
 
 
-    root
-      .nodes("else")
-      .collect(MoreCollectors.toOptional())
-      .ifPresent(elseNode -> {
-        final ElseBranch.Builder elseBranchBuilder = ElseBranch.builder();
-        this.parseApplicators(elseNode, elseBranchBuilder);
-        rootBuilder.branch(elseBranchBuilder.build());
-      });
-  }
+        root
+                .nodes("else")
+                .collect(MoreCollectors.toOptional())
+                .ifPresent(elseNode -> {
+                    final ElseBranch.Builder elseBranchBuilder = ElseBranch.builder();
+                    this.parseApplicators(elseNode, elseBranchBuilder);
+                    rootBuilder.branch(elseBranchBuilder.build());
+                });
+    }
 
-  private void parseApplicators(Node root, ConditionalBranch.Builder branchBuilder) {
-      root
-              .nodes("apply")
-              .collect(Collectors.toList())
-              .forEach(innerApplyNode -> {
+    private void parseApplicators(Node root, ConditionalBranch.Builder branchBuilder) {
+        root
+                .nodes("apply")
+                .collect(Collectors.toList())
+                .forEach(innerApplyNode -> {
 
-                  final Node[] innerIfNode = {null};
-                  List<Node> applicators = new ArrayList<>();
+                    final Node[] innerIfNode = {null};
+                    List<Node> applicators = new ArrayList<>();
 
-                  innerApplyNode.nodes().stream().forEach(node -> {
-                      if (node.name().equals("if")) {
-                          if (innerIfNode[0] == null) {
-                              innerIfNode[0] = node;
-                          } else {
-                              throw new IllegalArgumentException(String.format("Found more than one 'if' node: '%s' and '%s'", node, innerIfNode[0]));
-                          }
-                      } else {
-                          applicators.add(node);
-                      }
+                    innerApplyNode.nodes().stream().forEach(node -> {
+                        if (node.name().equals("if")) {
+                            if (innerIfNode[0] == null) {
+                                innerIfNode[0] = node;
+                            } else {
+                                throw new IllegalArgumentException(String.format("Found more than one 'if' node: '%s' and '%s'", node, innerIfNode[0]));
+                            }
+                        } else {
+                            applicators.add(node);
+                        }
 
-                  });
+                    });
 
-                  if (innerIfNode[0] != null) {
-                      final IfBranch.Builder ifBranchBuilder = IfBranch.builder();
-                      this.traverseIf(innerApplyNode, ifBranchBuilder);
-                      branchBuilder.branch(ifBranchBuilder.build());
-                  } else {
-                      // Safe to assume this is a result branch
-                      ResultBranch.Builder resultBranchBuilder = ResultBranch.builder();
-                      // TODO Apply Parsing Test Code
-                      for (Node applicatorNode: applicators) {
-                          resultBranchBuilder.apply(this.eventApplicatorParser.parse(applicatorNode));
-                      }
-                      //resultBranchBuilder.apply(new TransactionValidityApplicator(false));
-                      branchBuilder.branch(resultBranchBuilder.build());
-                  }
-              });
-  }
+                    if (innerIfNode[0] != null) {
+                        final IfBranch.Builder ifBranchBuilder = IfBranch.builder();
+                        this.traverseIf(innerApplyNode, ifBranchBuilder);
+                        branchBuilder.branch(ifBranchBuilder.build());
+                    } else {
+                        // Safe to assume this is a result branch
+                        ResultBranch.Builder resultBranchBuilder = ResultBranch.builder();
+                        // TODO Apply Parsing Test Code
+                        for (Node applicatorNode: applicators) {
+                            resultBranchBuilder.apply(this.eventApplicatorParser.parse(applicatorNode));
+                        }
+                        //resultBranchBuilder.apply(new TransactionValidityApplicator(false));
+                        branchBuilder.branch(resultBranchBuilder.build());
+                    }
+                });
+    }
 }
