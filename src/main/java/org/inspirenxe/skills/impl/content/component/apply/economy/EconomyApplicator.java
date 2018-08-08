2692b51e993dedb3e471926a6a557116c7012336
Aaron Hill <aa1ronham@gmail.com>
2018-08-08 11:31:00 -0400

    Make EconomyApplicator use Identifiable

--- src/main/java/org/inspirenxe/skills/impl/content/component/apply/economy/EconomyApplicator.java
+++ src/main/java/org/inspirenxe/skills/impl/content/component/apply/economy/EconomyApplicator.java
@@ -11,25 +11,24 @@ import org.spongepowered.api.event.Event;
 import org.spongepowered.api.service.economy.Currency;
 import org.spongepowered.api.service.economy.EconomyService;
 import org.spongepowered.api.service.economy.account.Account;
+import org.spongepowered.api.util.Identifiable;
 
-public class EconomyApplicator extends CauseFirstEventApplicator<Event, Player> {
+public class EconomyApplicator extends CauseFirstEventApplicator<Event, Identifiable> {
 
     private final MathOperation operation;
 
     protected EconomyApplicator(MathOperation operation) {
-        super(Player.class, Event.class);
+        super(Identifiable.class, Event.class);
         this.operation = operation;
     }
 
     @Override
-    protected void applyWithCause(EventData eventData, Event event, Player causeObject) {
+    protected void applyWithCause(EventData eventData, Event event, Identifiable causeObject) {
         Sponge.getServiceManager().provide(EconomyService.class).ifPresent(econ -> {
-            Skill skill = this.getSkill(eventData.getSkillType(), causeObject);
             Account account = econ.getOrCreateAccount(causeObject.getUniqueId()).get();
             Currency currency = econ.getDefaultCurrency();
 
             try (CauseStackManager.StackFrame frame = Sponge.getCauseStackManager().pushCauseFrame()) {
-                frame.pushCause(skill);
                 account.setBalance(currency, this.operation.apply(account.getBalance(currency)), frame.getCurrentCause());
             }
         });
